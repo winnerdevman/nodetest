ea44b6a8bde97e95def51fda853ab10f27335427
winwilust <winwilust@gmail.com>
2022-07-12 22:18:51 +0300

    update'

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+require('dotenv').config()
+
+const app = express()
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/index', function (req, res) {
+  // const signatureInfo = getSignatureFromPrivateKey(process.env.SIGNER_PRIVATE_KEY, ...req.query.params)
+  console.log(req.query); 
+  res.send(req.query); 
+})
+
+app.listen(8888)
+console.log('Listening to localhost:8888...');
